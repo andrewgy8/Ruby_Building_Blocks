93eb1ffc7a1e7cf732006fafa5acfa97ee9c19d8
andrewgy8 <andrewgy8@gmail.com>
2016-04-10 11:31:08 +0900

    added comments to the stock picker program

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,14 +1,18 @@
 def stock_picker(stocks)
+	
 	high = []
 	low = []
+	#puts first number in stock set to the high array
 	high.push(stocks[0])
-
+	#iterates through stock array to test whether they are higher in value than the current high array value
+	#if it is the high array is emptied, and the new value take its place
 	stocks.each do |stock|
 		if stock > high[0]
 			high.pop
 			high << stock
 		end
 	end
+	#same concept as the high array but low number instead
 	low.push(stocks[0])
 	stocks.each do |stock|
 		if stock < low[0]
@@ -16,11 +20,11 @@ def stock_picker(stocks)
 			low.push(stock)
 		end
 	end
-	
+	#finding the defference in the high and low array
 	high_low = high + low
 	puts high_low.inspect
-	sum = high_low[0] - high_low[1]
-	puts "The total money you could possibly make is " + sum.to_s
+	difference = high_low[0] - high_low[1]
+	puts "The total money you could possibly make is " + difference.to_s
 end
 
 stock_picker([18,3,6,9,15,8,6,45,10])
