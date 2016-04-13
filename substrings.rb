3de5ea90095934d0f6fc85b859e3ef8450942796
andrewgy8 <andrewgy8@gmail.com>
2016-04-13 23:15:48 +0900

    substrings

--- substrings.rb
+++ substrings.rb
@@ -3,25 +3,31 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 def substrings(word, dictionary)
 	#match hash
 	matches_hash = Hash.new(0)
+	#deletes all characters except those in the regexp
+	word = word.gsub(/[^0-9a-zA-Z]/, ' ')
 	#split input
-	split_word = word.split('')
+	split_word = word.downcase.split(' ')
 
 	#creates hashes match with 0 value
 	dictionary.each do |dict_word|
 		matches_hash[dict_word] += 0
 	end	
 	
-	#matches the input word to a key in the dictionary and assigns that putput array to a variable match
-	match = matches_hash.select {|k,v| k.match(word)}
-	
-	#merges both the match and dictionary with a value of 1 
-	matches_hash.merge!(match) {|k,v1,v2| v1 + (v2.to_i + 1)}
-	puts matches_hash
-
-	matches_hash.delete_if {|k,v| v < 1}
+	#matches the input word to a key in the dictionary and assigns that output array to a variable match
+	split_word.each do |words|
+		$positive_match = matches_hash.select {|k,v| k.match(words)}
+		
+		#adds one to the has value if there is a match found
+		$positive_match.update($positive_match){|k,v1| v1+1}
+		
+		#merges both the match and dictionary with a value of 1
+		matches_hash.merge!($positive_match).inspect 
+	end
+	matches_hash.delete_if {|k,v| v == 0}
 	puts matches_hash
 end
 
-substrings('below', dictionary)
+substrings('Below', dictionary)
+
 
 
