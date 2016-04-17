13a44c380235cc64d7b79f5d5b1c8e8ce979c706
andrewgy8 <andrewgy8@gmail.com>
2016-04-17 21:37:30 +0900

    Added bubble sort by

--- README.txt
+++ README.txt
@@ -2,7 +2,10 @@ These are the excerices from the RUBY section in the Odin Project
 Completed by Andrew Graham-Yooll
 Published to Git on Arpil 10, 2016
 Updated on April 13, 2016
+Updated on April 17, 2016
 Contains:
-Caesar Cipher
-stock picker
-substrings
+-Caesar Cipher
+-stock picker
+-substrings (this can probably be refactored)
+-bubble sort
+
--- bubble_sort.rb
+++ bubble_sort.rb
@@ -25,5 +25,27 @@ end
 
 bubbleSort(numbers)
 
+def bubbleSort_by(numbers)
+	#counter for while loop
+	sort = numbers.length
+	while sort > 0
+		#takes index and num and retruns accoringly the proper action
+		numbers.each_with_index do |num, index|
+			next_num = numbers[index+1]
+			if next_num.nil?
+				break
+			elsif yield(next_num, num) > 0
+				#will move larger number to index+1 location and then delete the original number
+				numbers.insert(next_num, numbers.delete_at(index))
+			else
+				#if both numbers are equal, it will pass
+				next
+			end
+			#puts numbers.inspect
+		end
+		sort -= 1
+	end
+	puts numbers.inspect
+end
 
-
+bubbleSort_by(numbers) {|left,right| right - left} 
