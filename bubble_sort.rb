1cf7258050e77a3ad58a64e15eeecd5ce8c2b18a
andrewgy8 <andrewgy8@gmail.com>
2016-04-17 21:23:54 +0900

    bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,29 @@
+numbers = [5,6,1,8,3,9,4,3]
+
+def bubbleSort(numbers)
+	#counter for while loop
+	sort = numbers.length
+	while sort > 0
+		#takes index and num and retruns accoringly the proper action
+		numbers.each_with_index do |num, index|
+			next_num = numbers[index+1]
+			if next_num.nil?
+				break
+			elsif num > next_num
+				#will move larger number to index+1 location and then delete the original number
+				numbers.insert(index+1, numbers.delete_at(index))
+			else
+				#if both numbers are equal, it will pass
+				next
+			end
+			#puts numbers.inspect
+		end
+		sort -= 1
+	end
+	puts numbers.inspect
+end
+
+bubbleSort(numbers)
+
+
+
