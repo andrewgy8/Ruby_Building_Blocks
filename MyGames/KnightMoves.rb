7207d13f3101545b4073ef9f184165d00188d531
andrewgy8 <andrewgy8@gmail.com>
2016-05-19 06:14:10 +0200

    Basic function of the knight moves

--- MyGames/KnightMoves.rb
+++ MyGames/KnightMoves.rb
@@ -1,56 +1,71 @@
+
+#--------------------------
+#----------Board-----------
+#--------------------------
+
+Board = Struct.new(:position, :y)
+
 #--------------------------
 #----Knight Movement-------
 #--------------------------
 
-class KnightMoves
-	
-	def knight_moves(finish)
-		#returns number of moves necessary to get to final position
-	end
-	
 
+def knight_moves(start, finish)
+	return "Sorry that is not a valid starting position." unless is_it_a_valid_move?(start)
+	return "Sorry that is not a valid finish position." unless is_it_a_valid_move?(finish)
+	return "You are ending at the same place you started.  Thats an easy problem." if start == finish
+
+	queue = [Board.new(start, [start])]
+
+	
+	visited = [start]
+
+	while !queue.empty?
+		
+		n = queue.shift
+		possible_moves = possible_moves(n.position).select {|move| !visited.include?(move)}
+
+		if possible_moves.include?(finish)
+			n.y << finish
+			if n.y.size - 1 == 1
+				puts "Your knight found its final position in #{n.y.size - 1} move and here is the path: #{n.y}."
+			else 
+				puts "You made it in #{n.y.size - 1} moves.  Here is your path: #{n.y}."
+			end
+			n.y.each {|position| puts position.inspect}
+		end
+
+		possible_moves.each do |move|
+			queue << Board.new(move, (n.y + [move]))
+			visited << move
+		end
+	end
+		#returns number of moves necessary to get to final position
 end
 
 #--------------------------
 #-----Knight Position------
 #--------------------------
+
 def is_it_a_valid_move?(move)
-	if (move[0] > -1 && move[0] < 8) && (move[1] > -1 && move[1] < 8)
-		return true
-	else
-		return false
-	end
+	return (move[0] > -1 && move[0] < 8) && (move[1] > -1 && move[1] < 8) ? true : false
 end
 
 def possible_moves(position)
 	possible_changes = [[2,1],[-2,1],[2,-1],[-2,-1],[1,2],[-1,2],[1,-2],[-1,-2]]
 	moves = []
-	possible_changes.each {|move| moves << [position[0]+move[0], position[1]+move[0]] }
-	moves.select do |move| 
-		unless is_it_a_valid_move?(move) 
-			moves.delete(move)
-		end
-	end
+	possible_changes.each {|move| moves << [position[0]+move[0], position[1]+move[1]] }
+	moves.select! {|pos| is_it_a_valid_move?(pos)}
 	moves
 end
-
-#--------------------------
-#----------Board-----------
-#--------------------------
-Board = Struct.new("Board", :x, :y)
-
-
-
 
 #--------------------------
 #----------Script----------
 #--------------------------
 
-#knight = Knight.new([0,0])
-#board_tree = build_tree(board)
-#look_tree(board_tree)
+#puts possible_moves([0,0])
+puts knight_moves([0,0], [7,7])
 
-possible_moves([0,0])
 
 
 
